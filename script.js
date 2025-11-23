329e34e22ad0b9cb198d56c398bca4c22e9f8e8f
tug <tuguldurganbold09@gmail.com>
2025-11-23 21:05:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 
+const posts = [
+    {
+        title: "1",
+        content: "2"
+    },
+    {
+        title: "3",
+        content: "4"
+    }
+];
+
+const container = document.getElementById("posts-container");
+
+function loadPosts() {
+    posts.forEach(post => {
+        const div = document.createElement("div");
+        div.className = "post";
+
+        div.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.content}</p>
+        `;
+
+        container.appendChild(div);
+    });
+}
+
+window.onload = loadPosts;
+
